98a45243efc115d8e0cd9c4e90361d4127b63b7e
Annie <annie.gallagher7@gmail.com>
2022-10-27 20:34:40 -0600

    added GET function for all books endpoint

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,7 @@
 from os import abort
 from app import db
 from app.models.book import Book
-from flask import Blueprint, jsonify, abort, make_response, request
+from flask import Blueprint, jsonify, make_response, request
 
 # class Book:
 #     def __init__(self, id, title, description):
@@ -19,7 +19,7 @@ from flask import Blueprint, jsonify, abort, make_response, request
 books_bp = Blueprint("books_bp", __name__, url_prefix="/books")
 
 @books_bp.route("", methods=["POST"])
-def handle_books():
+def create_book():
     request_body = request.get_json()
     new_book = Book(title=request_body["title"],
                     description=request_body["description"])
@@ -29,6 +29,19 @@ def handle_books():
 
     return make_response(f"Book {new_book.title} successfully created", 201)
 
+@books_bp.route("", methods=["GET"])
+def read_all_books():
+    books_response = []
+    books = Book.query.all()
+    for book in books:
+        books_response.append(
+            {
+                "id": book.id,
+                "title": book.title,
+                "description": book.description
+            }
+        )
+    return jsonify(books_response)
 #def validate_book(book_id):
 #    try:
 #        book_id = int(book_id)
@@ -45,7 +58,7 @@ def handle_books():
 # @books_bp.route("", methods=["GET"])
 # def handle_books():
 #     books_response = []
-#     for book in books:
+#     for book in book.query.all():
 #         books_response.append(
 #             {
 #                 "id": book.id,
