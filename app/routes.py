5a1890d4c8baba2526ebdd43a2dfc59ed313bc1f
Annie <annie.gallagher7@gmail.com>
2022-11-02 09:17:36 -0600

    fixed bug on line 15 that had book instead of model

--- app/routes.py
+++ app/routes.py
@@ -12,7 +12,7 @@ def validate_model(cls, model_id):
 
     model = cls.query.get(model_id)
 
-    if not book:
+    if not model:
         abort(make_response({"message":f"{cls.__name__} {model_id} not found"}, 404))
 
     return model
