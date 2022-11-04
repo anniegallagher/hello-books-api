4c9fe0d9588f5ba53c5802746b27a93beab28b97
Annie <annie.gallagher7@gmail.com>
2022-11-04 11:20:48 -0600

    updated registration of blueprints due to update in models layout

--- app/__init__.py
+++ app/__init__.py
@@ -29,10 +29,16 @@ def create_app(test_config=None):
     migrate.init_app(app, db)
 
     # Register Blueprints here
-    from .book_routes import books_bp
-    app.register_blueprint(books_bp)
+    # from .book_routes import bp as books_bp
+    # app.register_blueprint(books_bp)
 
-    from .author_routes import authors_bp
-    app.register_blueprint(authors_bp)
+    # from .author_routes import bp as authors_bp
+    # app.register_blueprint(authors_bp)
+
+    # Register blueprints
+    from .routes import book
+    from .routes import author
+    app.register_blueprint(book.bp)
+    app.register_blueprint(author.bp)
 
     return app
